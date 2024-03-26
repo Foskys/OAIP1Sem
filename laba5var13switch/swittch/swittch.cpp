a6d1d028036358c93bd22c92a9e231dd84e5b210
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:52:11 +0300

    Add files via upload

---
+++ laba5var13switch/swittch/swittch.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	int k;
+	puts("Ты трапезничал сегодня? (1-Да, 2-Нет)");
+	cin >> k;
+	switch (k)
+	{
+	case 1:
+	{puts("Что ты кушал? (1-Бутерброды, 2-Салат)");
+	cin >> k;
+	switch (k)
+	{
+	case 1:
+	{
+		puts("Это очень вредно для здоровья, ты знаешь об этом? (1-Да, 2-Нет, я ем их каждый день");
+		cin >> k;
+		switch (k)
+		{
+			case 1:
+			{
+				puts("А стакан воды с утра пил? (1-Да, 2-Нет)");
+				cin >> k;
+				switch (k)
+				{
+					case 1: puts("Хорошо"); break;
+					case 2: puts("Выпей сейчас, я принесу тебе"); break;
+				}
+				break;
+			}
+			case 2: puts("очень плохо"); break;
+		}
+		break;
+	}
+	case 2:puts("это очень полезно"); break;
+	}
+	break;
+	}
+	case 2: puts("Надо покушать"); break;
+	default: puts("Некорректный вариант"); break;
+	}
+	return 0;
+
+}
+
+
