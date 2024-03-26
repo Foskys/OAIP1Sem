39d232ed1a4e523cbd5b7636715907c2218ff9c3
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:48:03 +0300

    Add files via upload

---
+++ laba4var15/15-1/15-1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <iomanip>
+ void main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	char c, space; space = ' ';
+	cout << "Введите символ ";
+	cin >> c;
+	cout << setw(40) << setfill(space) << space;
+	cout << setw(2)  << setfill(c) << c << endl;
+	cout << setw(38) << setfill(space) << space;
+	cout << setw(6)  << setfill(c) << c << endl;
+	cout << setw(36) << setfill(space) << space;
+	cout << setw(10) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(space) << space;
+	cout << setw(14) << setfill(c) << c << endl;
+	cout << setw(32) << setfill(space) << space;
+	cout << setw(18) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(space) << space;
+	cout << setw(22) << setfill(c) << c << endl;
+	cout << setw(28) << setfill(space) << space;
+	cout << setw(26) << setfill(c) << c << endl;
+	cout << setw(26) << setfill(space) << space;
+	cout << setw(30) << setfill(c) << c << endl;
+	cout << setw(24) << setfill(space) << space;
+	cout << setw(34) << setfill(c) << c << endl;
+
+	cout << setw(30) << setfill(space) << space;
+	cout << setw(22) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(space) << space;
+	cout << setw(22) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(space) << space;
+	cout << setw(22) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(space) << space;
+	cout << setw(22) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(space) << space;
+	cout << setw(22) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(space) << space;
+	cout << setw(22) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(space) << space;
+	cout << setw(22) << setfill(c) << c << endl;
+
+
+
+
+
+
+}
---
+++ laba4var15/15-2/15-2/15-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	double n;
+	cout << "Введите n - ";
+	cin >> n;
+	if (n <= 36 and n > 0)
+	{
+		double Квартира = n, Этаж = ceil(n / 4);
+		cout << "Квартира - " << Квартира << "\n";
+		cout << "Этаж - " << Этаж << endl;
+	}
+	else
+	{
+		cout << "Ошибка" << endl;
+	}
+	return(0);
+}
