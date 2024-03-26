aad722bf752d0861c1464944e45bb68e56a78e15
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:41:52 +0300

    Add files via upload

---
+++ laba3var13/13.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double t, y, d = 0.5e-8, c = -9, a = 1.5;
+	const double e = 2.71828;
+	t = (d * c + a * sqrt(abs(c - 1)));
+	y = 0.5*t / d + pow(e, a);
+	cout << "t=" << t << '\n';
+	cout << "y=" << y;
+}
