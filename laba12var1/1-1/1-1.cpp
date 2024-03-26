c769ba25580be72fbf18d85c07cb7cb036f0f0e0
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 22:01:10 +0300

    Add files via upload

---
+++ laba12var1/1-1/1-1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	int y, m, ost = 0, count = 0;
+	int*arr = new int[100];
+	cout << "Input y: ";
+	cin >> y;
+	cout << "Input m: ";
+	cin >> m;
+	while (y > 9)
+	{
+		ost = y % 10;
+		y = (y - ost) / 10;
+		if (ost % m == 0)
+		{
+			arr[count] = ost;
+			count++;
+		}
+		else { continue; }
+	}
+	for (int j = 0; j < count; j++) {
+		cout << arr[j] << " ";
+	}
+	return 0;
+}
