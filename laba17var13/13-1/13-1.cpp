088a0c3b210415c985401533295e02336e1cd91a
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 22:51:49 +0300

    Add files via upload

---
+++ laba17var13/13-1/13-1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+void massivB(float* A, float* B, int& n, float& sum_kv, int& j) {
+	using namespace std;
+	int i;
+	for (i = 0, j = 0; i < n; i++)
+	{
+		if (i % 2 == 0 && A[i] > 0)
+		{
+			B[j] = A[i];
+			j++;
+		}
+	}
+	for (i = 0; i < j; i++)
+		sum_kv += B[i] * B[i];
+}
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	float* A, * B, sum_kv = 0;
+	int n = 21, i, j;
+	A = new float[n];
+	B = new float[n];
+	cout << "Введите 20 положительных и отрицательных чисел" << endl;
+	for (i = 0; i < n; i++)
+	{
+		cout << "Элемент " << i << ": ";
+		cin >> A[i];
+	}
+	cout << "A = {";
+	for (i = 0; i < n; i++)
+	{
+		cout << A[i];
+		if (i < n - 1)
+		{
+			cout << ", ";
+		}
+	}
+	cout << "}" << endl;
+	massivB(A, B, n, sum_kv, j);
+	cout << "Массив В сформирован из положительных элементов массива А, имеющих чётный индекс" << endl;
+	cout << "B = {";
+	for (i = 0; i < j; i++)
+	{
+		cout << B[i];
+		if (i < j - 1)
+		{
+			cout << ", ";
+		}
+	}
+	cout << "}" << endl;
+	cout << "Сумма квадратов элементов равна sum_kv = " << sum_kv << endl;
+	delete[] B;
+	delete[] A;
+}
---
+++ laba17var13/13-2/13-2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream> 
+void min_zn(int** A, int& min_m, int& min_d, int& k, int& m, int& n, int& ind) {
+	min_m = A[0][0];
+	for (k = 0; k < n; k++)
+	{
+		for (m = 0; m < n; m++)
+		{
+			if (*(*(A + k) + m) < min_m)
+				min_m = *(*(A + k) + m);
+		}
+	}
+	min_d = A[0][0];
+	for (k = 0; k < n; k++)
+	{
+		if (*(*(A + k) + k) < min_d)
+		{
+			min_d = *(*(A + k) + k);
+			ind = k;
+		}
+	}
+}
+int main() {
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	int** A, i, n, k, m, min_m, min_d, ind;
+	cout << "Введите количество строк и столбцов квадратной матрицы: ";
+	cin >> n;
+	A = new int*[n];
+	for (i = 0; i < n; i++)
+	{
+		*(A + i) = new int[n];
+	}
+	srand((unsigned)time(NULL));
+	cout << "Матрица:" << endl;
+	for (k = 0; k < n; k++)
+	{
+		for (m = 0; m < n; m++)
+		{
+			*(*(A + k) + m) = rand() % 51;
+			cout << *(*(A + k) + m) << ' ';
+			if (*(*(A + k) + m) < 10)
+				cout << " ";
+			if (m == n - 1)
+				cout << endl;
+		}
+	}
+	min_zn(A, min_m, min_d, k, m, n, ind);
+	if (min_d == min_m)
+	{
+		cout << "Минимальное значение матрицы а = " << min_m << " находится на главной диагонали" << endl;
+		cout << "Индексы этого элемента " << ind << "," << ind << endl;
+	}
+	else cout << "Минимальное значение матрицы не находится на главной диагонали" << endl;
+	for (i = 0; i < n; i++)
+	{
+		delete* (A + i);
+	}
+	delete[] A;
+}
