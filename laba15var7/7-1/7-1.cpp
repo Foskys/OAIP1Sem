5d3c58198c4ef4f5dd790d1fbaed85d3e6e8f7c9
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 22:39:41 +0300

    Add files via upload

---
+++ laba15var7/7-1/7-1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>;
+using namespace std;
+int main() {
+	int* a, n, total = 0, amax = 0;
+	cout << " n = "; 
+	cin >> n;
+	setlocale(LC_ALL, "rus");
+	if (!(a = (int*)malloc(n * sizeof(int)))) 
+	{
+		cout << "overload" << endl;
+		return 0; 
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "arr el: ";
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		total += a[i];
+	}
+	total = total / n;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > total)
+		{
+			amax += 1;
+		}
+	}
+	cout << amax << " числа больше среднего арифметического" << endl;
+	free(a);
+	system("pause");
+	return 0;
+}
---
+++ laba15var7/7-2 popitka/7-2 popitka.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>;
+using namespace std;
+int m, n, strind, ** matrix, zcount = 0, dublic = 0, xbad, xmax, xmaxbuff;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << " n = ";
+	cin >> n;
+	cout << " m = ";
+	cin >> m;
+	cout << endl;
+	matrix = new int* [n];
+	strind = n;
+
+	for (int istr = 0; istr < n; istr++)
+	{
+		matrix[istr] = new int[m];
+
+		for (int icol = 0; icol < m; icol++)
+		{
+
+			cin >> matrix[istr][icol];
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int istr = 0; istr < n; istr++) {
+		for (int icol = 0; icol < m; icol++)
+		{
+
+			cout << matrix[istr][icol] << endl;
+
+		}
+		cout << endl;
+	}
+	xmax = INT_MIN;
+	for (int icol = 0; icol < m; icol++)
+	{
+		for (int istr = 0; istr < n; istr++)
+		{
+			if (matrix[icol][istr] == 0) { strind--; break; }
+		}
+	}
+	for (int icol = 0; icol < m; icol++)
+	{
+		for (int istr = 0; istr < n; istr++)
+		{
+			if (matrix[icol][istr] > xmax) { xmax = matrix[icol][istr]; dublic = 0; }
+			if (matrix[icol][istr] == xmax) { dublic++; }
+		}
+	}
+	while (dublic < 2)
+	{
+		xbad = xmax;
+		xmax = INT_MIN;
+		for (int icol = 0; icol < m; icol++)
+		{
+			for (int istr = 0; istr < n; istr++)
+			{
+				if ((matrix[icol][istr] > xmax) && (matrix[icol][istr]) < xbad) { xmax = matrix[icol][istr]; dublic = 0; }
+				if (matrix[icol][istr] == xmax) { dublic++; }
+			}
+		}
+		if (xmax == INT_MIN) { dublic = 3; }
+	}
+	cout << "non null " << strind << endl;
+	if (xmax == INT_MIN)
+	{
+		cout << "Отсутствуют повторы чисел" << endl;
+	}
+	else
+	{
+		cout << "xmax " << xmax << endl;
+	}
+	delete[] matrix;
+	system("pause");
+	return 0;
+}
