87b515995482337c27e9ebe9e27917ba2fc71a82
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 21:39:08 +0300

    Add files via upload

---
+++ laba11var13/13-1/13-1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	char tmp[33];
+	int A, B, maskA = 48;
+	cout << "Первое число А = ";
+	cin >> A;
+	cout << "Второе число В = ";
+	cin >> B;
+	_itoa_s(A, tmp, 2);
+	cout << "A (в двоичной системе) = " << tmp << endl;
+	_itoa_s(B, tmp, 2);
+	cout << "B (в двоичной системе) = " << tmp << endl;
+	int read = A & maskA;
+	int clearMask = ~maskA;
+	B = B & clearMask;
+	B = B | read;
+	_itoa_s(B, tmp, 2);
+	cout << "Новое B = " << B << endl;
+	cout << "Новое B (в двоичной системе) = " << tmp << endl;
+}
---
+++ laba11var13/13-2/13-2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	char tmp[33];
+	int A, n, p, mask = 0, i;
+	cout << "Введите число A = ";
+	cin >> A;
+	cout << "Введите число битов, которые нужно инвертировать в 1 n = ";
+	cin >> n;
+	cout << "Введите позицию, справа от которой будет происходить инвертирование p = ";
+	cin >> p;
+	_itoa_s(A, tmp, 2);
+	cout << "A (в двоичной системе) = " << tmp << endl;
+	for (i = 0; i < n; i++)
+	{
+		mask = mask | 1;
+		mask = mask << 1;
+	}
+	mask = mask >> 1;
+	mask = mask <<(p - n - 1);
+	cout << "Mask = " << mask << endl;
+	_itoa_s(mask, tmp, 2);
+	cout << "Mask binary = " << tmp << endl;
+	A = A | mask;
+	cout << "A инвертированное = " << A << endl;
+	_itoa_s(A, tmp, 2);
+	cout << "A инвертированное (в двоичной системе) = " << tmp << endl;
+}
