6cab813e2bf45492531a4c179d4b3f84a1de777e
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 21:47:16 +0300

    Add files via upload

---
+++ laba12var13/13-1/13-1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+ int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	const int maxSize = 100;
+	int u, i, kol = 0, X[maxSize];
+	double n;
+	bool a;
+	cout << "Введите размер массива (0 < n <= 100) ";
+	cin >> n;
+	while (n < 1 || n - (int)n != 0 || n > maxSize)
+	{
+		cout << "Число не удовлетворяет условию, введите другое число" << endl;
+		cin >> n;
+	}
+	srand((unsigned)time(NULL));
+	cout << endl << "Массив Х = {";
+	for (u = 0; u <= n - 1; u++)
+	{
+		*(X + u) = rand() % 50;
+		cout << " " << * (X + u);
+		if (u < n - 1)
+			cout << ';';
+	}
+	cout << "}" << endl;
+	cout << "Количество неповторяющихся элементов = ";
+	for (u = 0; u <= n - 1; u++)
+	{
+		a = true;
+		for (i = 0; i <= n - 1; i++)
+		{
+			if (*(X + u) == *(X + i) && i != u)
+				a = false;
+		}
+		if (a == true)
+			kol++;
+	}
+	cout << kol;
+}
---
+++ laba12var13/13-2/13-2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	const int maxSize = 100;
+	int u, i, X[maxSize], t;
+	double n, sr_ar = 0;
+	cout << "Введите размер массива (0 < n <= 100) ";
+	cin >> n;
+	while (n < 1 || n - (int)n != 0 || n > maxSize)
+	{
+		cout << "Число не удовлетворяет условию, введите другое число" << endl;
+		cin >> n;
+	}
+	srand((unsigned)time(NULL));
+	cout << endl << "Массив Х = {";
+	for (u = 0; u <= n - 1; u++)
+	{
+		*(X + u) = rand() % 50;
+		cout << " " << * (X + u);
+		if (u < n - 1)
+			cout << ';';
+	}
+	cout << "}" << endl;
+	cout << "Среднее арифметическое элементов массива = ";
+	for (u = 0; u <= n - 1; u++)
+		sr_ar += *(X + u);
+	sr_ar /= n;
+	cout << sr_ar;
+	for (u = 0; u <= n - 1; u++)
+	{
+		if (*(X + u) < sr_ar)
+		{
+			t = *(X + u);
+			for (i = u; i > 0; i--)
+			{
+				*(X + i) = *(X + i - 1);
+			}
+			*X = t;
+		}
+	}
+	cout << endl << "Новый массив Х = {";
+	for (u = 0; u <= n - 1; u++)
+	{
+		cout << " " << * (X + u);
+		if (u < n - 1)
+			cout << ';';
+	}
+	cout << "}" << endl;
+}
