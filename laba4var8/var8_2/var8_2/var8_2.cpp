251ee559d6042dd1bfe33d5a9950c40c9c9b37f0
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:44:22 +0300

    Add files via upload

---
+++ laba4var8/var8_2/var8_2/var8_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	double a, b, c, p, s;
+	cout << "Введите стороны треугольника: a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+	if (a < b + c || b < a + c || c < a + b)
+	{
+		p = (a + b + c) / 2;
+		s = sqrt(p * (p - a) * (p - b) * (p - c));
+		cout << "Площадь треугольника = " << s << endl;
+		return (0);
+	}
+	else
+	{
+		cout << "Ошибка ";
+	}
+}
