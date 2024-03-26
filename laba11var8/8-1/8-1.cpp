4223ee2275429660ef60cdca2e04f19d5c77e501
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 21:40:39 +0300

    Add files via upload

---
+++ laba11var8/8-1/8-1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+
+{
+	setlocale(LC_CTYPE, "Russian");
+	int A, B = 3067833783; char tmp[33];
+	cout << " Введите число А: ";
+	cin >> A;
+	_itoa_s(A, tmp, 2);
+	cout << " Число А: " << tmp << endl;
+	_itoa_s(B, tmp, 2);
+	cout << " Маска для А: " << tmp << endl;
+	_itoa_s(A & B, tmp, 2);
+	cout << " Результат: " << tmp << endl;
+	return 0;
+}
---
+++ laba11var8/8-2/8-2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	char tmp[33];
+	int A, B, n, m;
+	cout << "Первое число = ";
+	cin >> A;
+	cout << "Второe число = ";
+	cin >> B;
+	_itoa_s(A, tmp, 2);
+	cout << "A = " << tmp << endl;
+	_itoa_s(B, tmp, 2);
+	cout << "B = " << tmp << endl;
+	cout << "Число n = "; 
+	cin >> n;
+	cout << "Число m = ";
+	cin >> m;
+	int maskA = (A >> n) & 7;
+	int maskB = ~(7 << m);
+	_itoa_s((B & maskB) | (maskA << m), tmp, 2); 
+	cout << "B = " << tmp << endl;
+	return 0;
+}
