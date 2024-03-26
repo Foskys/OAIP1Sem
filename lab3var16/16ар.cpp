5122b35326ec2e4c7b5f8d45bae329ad8601f3fa
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:38:10 +0300

    Add files via upload

---
+++ lab3var16/16ар.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		long double y, w, z = 0.05e-5, m = 6;
+		const double e = 2.71828;
+		y = cos(5 * m) / pow(sin(0.4 * m), 2);
+		w = 4 * z * y - 7 * pow(e, -2 * y);
+		cout.unsetf(ios::dec);
+		cout.setf(ios::oct);
+		cout << "y=" << y << '\n';
+		cout << "w=" << w;
+		return 0;
+
+}
+
