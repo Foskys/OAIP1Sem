5a573f57670b203b72c3d75c804eb6b3634b61c3
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 22:37:35 +0300

    Add files via upload

---
+++ laba15var9/9-1/9-1.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
+	int size;
+	cout << "Введите размерность массива: ";
+	cin >> size; 
+	cout << endl;
+
+	int* arr = new int[size];
+
+	cout << "Массив: " << endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	int min_size = arr[0], min_size_num;
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] <= min_size)
+		{
+			min_size = arr[i];
+			min_size_num = i;
+
+		}
+	}
+	cout <<endl;
+	cout << "Минимальный элемент = " << min_size << ", который находится под номером: " << min_size_num + 1 << endl;
+
+	int sum = 0, mult = 1;
+
+	for (int i = 0; i < min_size_num; i++)
+	{
+		sum += arr[i];
+		mult *= arr[i];
+	}
+	cout << endl;
+	cout << "Сумма элементов до " << min_size_num + 1 << " номера равна = " << sum << ", произведение = " << mult << endl;
+	cout << endl;
+
+	delete[] arr;
+}
---
+++ laba15var9/9-2/9-2.cpp
@@ -0,0 +1,86 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
+
+	const int row = 5;
+	const int col = 5;
+	int arr[row][col];
+
+	cout << "\t Матрица: " << endl;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			arr[i][j] = rand() % 15 - 5;
+		}
+	}
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << "\n";
+	}
+
+	int num_row = 0;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			if (arr[i][j] < 0)
+			{
+				break;
+			}
+
+			else
+			{
+				num_row = i;
+				goto active;
+			}
+		}
+
+	}
+
+active:
+	int sum = 0;
+
+	for (int i = num_row; i <= num_row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			sum += arr[i][j];
+		}
+	}
+	cout << "\n Сумма элементов " << num_row + 1 << " строки = " << sum << endl;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			arr[i][j] -= sum;
+		}
+	}
+
+	cout << "\n \t Новая матрица: \n" << endl;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << "\n";
+	}
+
+
+	cout << endl;
+
+}
