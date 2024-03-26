87f3cc74d05c16f8b67bbad200633092984beeb7
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 22:36:41 +0300

    Add files via upload

---
+++ laba15var13/13-1/13-1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	float* mas, sum1 = 0, sum2 = 0;
+	int n, i, el1 = -1, el2;
+	cout << "Введите размер массива: ";
+	cin >> n;
+	mas = (float*)malloc(n * sizeof(float));
+	for (i = 0; i < n; i++)
+	{
+		cout << "Элемент " << i << ": ";
+		cin >> mas[i];
+	}
+	cout << "mas = {";
+	for (i = 0; i < n; i++)
+	{
+		cout << mas[i];
+		if (i < n - 1)
+		{
+			cout << ", ";
+		}
+	}
+	cout << "}" << endl;
+	for (i = 0; i < n; i++)
+	{
+		if (mas[i] < 0)
+		{
+			if (el1 == -1)
+			{
+				el1 = i;
+			}
+			el2 = i;
+		}
+	}
+	cout << "Индекс первого отрицательного элемента: " << el1 << endl;
+	cout << "Индекс последнего отрицательного элемента: " << el2 << endl;
+	for (i = 0; i < n; i++)
+	{
+		if (i % 2 != 0)
+		{
+			sum1 += mas[i];
+		}
+	}
+	cout << "Сумма элементов с нечетными номерами: " << sum1 << endl;
+	for (i = el1 + 1; i < el2; i++)
+	{
+		sum2 += *(mas + i);
+	}
+	cout << "Сумма элементов между первым и последним отрицательными номерами: " << sum2 << endl;
+	free(mas);
+}
---
+++ laba15var13/13-2/13-2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	float** A;
+	int n, m, i, j, str = 0;
+	bool nuli;
+	cout << "Введите число строк: ";
+	cin >> n;
+	cout << "Введите число столбцов: ";
+	cin >> m;
+	A = new float* [n];
+	for (i = 0; i < n; i++)
+	{
+		*(A + i) = new float[m];
+	}
+	srand(time(NULL));
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			*(*(A + i) + j) = rand() % 4;
+		}
+	}
+	cout << "Матрица:" << endl;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			cout <<* (*(A + i) + j) << " ";
+		}
+		cout << endl;
+	}
+	for (i = 0; i < n; i++)
+	{
+		nuli = true;
+		for (j = 0; j < m; j++)
+		{
+			if (*(*(A + i) + j) != 0)
+			{
+					nuli = false;
+					break;
+			}
+		}
+		if (nuli == true)
+		{
+			str = i;
+			break;
+		}
+	}
+	if (nuli == false)
+		cout << "Строки с нолями нет";
+	else {
+		cout << "Строка со всеми нулями под номером " << str + 1 << endl;
+		for (i = 0; i < n; i++)
+		{
+			*(*(A + i) + str) /= 2;
+		}
+		cout << "Полученная матрица:" << endl;
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < m; j++)
+			{
+				cout <<* (*(A + i) + j) << " ";
+			}
+			cout << endl;
+		}
+	}
+	for (i = 0; i < n; i++)
+	{
+		delete* (A + i);
+	}
+	delete[] A;
+}
