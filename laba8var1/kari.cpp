a2b4425d24f89291896e5361fb8579f6cc7911e9
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 19:13:16 +0300

    Add files via upload

---
+++ laba8var1/kari.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	int i;
+	float x[10], z, y;
+	for (i = 0; i < 5; i++)
+	{
+		cout << "Введите " << i+1 << "-е значение y: " << endl;
+		cin >> x[i];
+	}
+	z = x[0];
+	for (i = 1; i < 5; i++)
+	{
+		if (x[i] > z)
+			z = x[i];
+	}
+	y = 0;
+	for (i = 0; i < 5; i++)
+		y += z * (pow(x[i], 2));
+	cout << "максимум- " << z << endl << "Сумма - " << y << endl;
+
+	return 0;
+}
