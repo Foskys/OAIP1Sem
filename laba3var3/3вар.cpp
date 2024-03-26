db66eb95aff54475ca231887697bf4d3775d60f5
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:37:00 +0300

    Add files via upload

---
+++ laba3var3/3вар.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		double f, d, z = 1.5e-8, i = -6, x = 4.5;
+		d = tan(-x * i) / sqrt(x - z);
+		f = sin(2 * d) / d;
+		cout << "d=" << d << '\n';
+		cout << "f=" << f;
+	
+}
+
