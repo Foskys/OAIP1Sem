6bd21b2f820c034959a3b0a5de0b508780fa9990
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 22:34:44 +0300

    Add files via upload

---
+++ laba14var13/13-1/1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	const int Max_n = 100;
+	int C[Max_n][Max_n];
+	int n, k, m, min, stolbec;
+	cout << "Введите количество строк и столбцов квадратной матрицы: ";
+	cin >> n;
+	srand((unsigned)time(NULL));
+	cout <<"Матрица:" << endl;
+	for (k = 0; k < n; k++)
+	{
+		for (m = 0; m < n; m++)
+		{
+			C[k][m] = rand() % 51;
+			cout << C[k][m] << ' ';
+			if (C[k][m] < 10)
+				cout << " ";
+			if (m == n - 1)
+				cout << endl;
+		}
+	}
+	min = C[0][0];
+	stolbec = 0;
+	for (k = 0; k < n; k++)
+	{
+		for (m = 0; m < n; m++)
+		{
+			C[k][m] = rand() % 51;
+			cout << C[k][m] << ' ';
+			if (C[k][m] < 10)
+				cout << " ";
+			if (m == n - 1)
+				cout << endl;
+		}
+	}
+	min = C[0][0];
+	stolbec = 0;
+	for (k = 0; k < n; k++)
+	{
+		if (C[k][k] < min)
+		{
+			min = C[k][k];
+			stolbec = k;
+		}
+	}
+	cout << "Наименьший элемент главной диагонали матрицы - C[" << stolbec + 1 << "; " << stolbec + 1 << "] = " << min;
+	cout<<"Столбец, в котором находится наименьший элемент главной диагонали матрицы: " << endl;
+	for (k = 0; k < n; k++)
+		cout << C[k][stolbec] << endl;
+}
---
+++ laba14var13/13-2/13-2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");	
+	const int Max_n = 50, Max_m = 50;
+	int C[Max_n][Max_n];
+	int n, m, k, j, kol = 0, sum;
+	cout << "Введите количество строк матрицы: ";
+	cin >> n;
+	cout << "Введите количество столбцов матрицы: ";
+	cin >> m;
+	srand((unsigned)time(NULL));
+	cout << "Матрица:" << endl;
+	for (k = 0; k < n; k++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			*(*(C + k) + j) = -50 + rand() % 101;
+			if ((*(*(C + k) + j) > -10 && *(*(C + k) + j) < 0) || (*(*(C + k) + j) > 9))
+				cout << " ";
+			if (*(*(C + k) + j) > -1 && *(*(C + k) + j) < 10)
+				cout << " ";
+			cout <<* (*(C + k) + j) << ' ';
+			if (j == m - 1)
+				cout << endl;
+		}
+	}
+	for (j = 0; j < m; j++)
+	{
+		sum = 0;
+		for (k = 0; k < n; k++)
+		{
+			sum += *(*(C + k) + j);
+		}
+		for (k = 0; k < n; k++)
+		{
+			if ((sum > 0 && *(*(C + k) + j) > sum - abs(*(*(C + k) + j))) || (sum < 0 && *(*(C + k) + j) > sum + abs(*(*(C + k) + j))))
+				kol++;
+		}
+	}
+	cout << "Количество элементов матрицы, значение каждого из которых больше суммы остальных элементов своего столбца - " << kol;
+}
