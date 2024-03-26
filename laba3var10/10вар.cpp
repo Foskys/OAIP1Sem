6598e15f6904f7e63ff221cc66b91a5a4d732be3
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:40:46 +0300

    Add files via upload

---
+++ laba3var10/10вар.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double y, s, z = 1.7, a = 4e-8, m = 3, n = 3;
+	const double e = 2.71828;
+	y = (z + log(z)) / (exp(-3) * sqrt(a));
+	s = (1 + m * n) / log(1 + z);
+	cout << "y=" << y << '\n';
+	cout << "s=" << s;
+}
+
