9ad578c37cc5a2f13b1890d02dea21d49c74977e
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 22:38:40 +0300

    Add files via upload

---
+++ laba15var8/8-1/8-1.cpp
@@ -0,0 +1,67 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
+	int size;
+	cout << "Введите размерность массива: ";
+	cin >> size;
+	int* arr = new int[size];
+
+	cout << endl;
+	cout << "Массив: " << endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10 - 5;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
+	int k = 0;
+	for (int i = 0; i < size; i++) /*количество положительных чисел*/
+	{
+		if (arr[i] > 0)
+		{
+			k++;
+		}
+	}
+		int number = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == 0)
+		{
+			number = i;
+		}
+	}
+
+	if (number == 0)
+	{
+		cout << endl;
+		cout << "Количество положительных чисел = " << k << endl;
+		cout << "Сумма элементов после нуля = 0 " << endl;
+	}
+	else
+	{
+		int sum = 0;
+
+		for (number; number < size; number++) /*сумма элементов после нуля*/
+		{
+			sum += arr[number];
+		}
+
+		cout << endl;
+		cout << "Количество положительных чисел = " << k << endl;
+		cout << "Сумма элементов после нуля = " << sum << endl;
+	}
+
+	delete[] arr;
+}
---
+++ laba15var8/8-2/8-2.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	const int row = 5;
+		const int col = 5;
+	int arr[row][col];
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			arr[i][j] = rand() % 5 - 5;
+		}
+	}
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << "\n";
+	}
+
+	int num_col = 0;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			if (arr[i][j] == 0)
+			{
+				num_col = j + 1;
+				break;
+			}
+		}
+	}
+	cout << endl;
+
+	cout << " Номер первого из столбцов, содержащих хотя бы один нулевой элемент равен " << num_col << endl;
+
+	cout << endl;
+}
