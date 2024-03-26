cb693e1463787fe80798af897e66a82c0de27413
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:57:29 +0300

    Add files via upload

---
+++ var7izlab2/7.lab2/7.lab2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if (a < b && b < c) {
+		a = c;
+		b = c;
+	}
+	else if (a > b && b > c) {
+		// do nothing
+	}
+	else {
+		a = a * a;
+		b = b * b;
+		c = c * c;
+	}
+
+	cout << a << " " << b << " " << c << endl;
+
+	return 0;
+}
