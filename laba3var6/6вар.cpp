f8aed025e88d5a950115824ddd0df698a0e82a00
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 18:39:38 +0300

    Add files via upload

---
+++ laba3var6/6вар.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double t, u, s, y= 0.956, a = 5e-6, n = 4;
+	const double e = 2.71828;
+	using namespace std;
+	t = 1 / sqrt(y) + 14 * a;
+	u = (t + 1) / (a + 2);
+	s = log((2 * n / 3) + pow(e, -n) / u);
+	cout << "\n t=" << t;
+	cout << "\n u=" << u;
+	cout << "\n s=" << s;
+}
+
