f5443a09c96290f41cd0e85cdd07c69ac1c0de35
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 19:05:09 +0300

    Add files via upload

---
+++ laba7var13/13-1/13-1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stdio.h>
+void main()
+{
+	float t, x, b = 3, i = 8, c = 6e-4, a = 6;
+	while (i < 25) {
+		t = a * i / (pow(a, 2) - b) * exp(-a);
+		printf("i=%5.2f\t", i);
+		printf("t=%5.2f\t", t);
+		if (t > 5 * c)
+			x = 4.8e-3 + i * a;
+		if (t <= 5 * c)
+			x = a + pow(i, 2) * t;
+		printf("x=%6.4f\n", x);
+		i = i + 4;
+
+	}
+}
---
+++ laba7var13/2/2/2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int count = 0;
+	int number = 1;
+
+	while (count < 6) {
+		if (number % 3 == 0 && number % 5 == 0) {
+			cout << number << " ";
+			count++;
+		}
+		number++;
+	}
+
+	return 0;
+}
