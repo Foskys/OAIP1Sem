4d291131cd1cf1c25fced0299759329e5d43af8d
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 21:34:51 +0300

    Add files via upload

---
+++ laba10var13/1/1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <locale>
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	const int maxSize = 100;
+	int A[maxSize], i, s = 0, n;
+	cout << "Введите размер массива (0 < n <= 100) ";
+	cin >> n;
+	srand((unsigned)time(NULL));
+	cout << "Исходный массив A = {";
+	for (i = 0; i <= (n - 1); i++)
+	{
+		A[i] = rand() % 100;
+		cout << " " << A[i];
+		if (i < n - 1)
+			cout << ';';
+	}
+	cout << "}";
+	for (i = 1; i <= (n - 1); i++)
+	{
+		s += A[i - 1];
+		A[i] = s;
+	}
+	cout << endl << "Новый массив A = {";
+	for (i = 0; i <= (n - 1); i++)
+	{
+		cout << " " << A[i];
+		if (i < n - 1)
+
+			cout << ';';
+	}
+	cout << "}";
+}
---
+++ laba10var13/2/2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <locale>
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	const int maxSize = 100;
+	char A[maxSize];
+	int u, i;
+	double k;
+	bool a;
+	cout << "Введите размер массива (0 < k <= 100) ";
+	cin >> k;
+	while (k < 1 || k - (int)k != 0 || k > maxSize)
+	{
+		cout << "Число не удовлетворяет условию, введите другое число" << endl;
+		cin >> k;
+	}
+	srand((unsigned)time(NULL));
+	cout << endl << "Исходный массив A = {";
+	for (u = 0; u <= k - 1; u++)
+	{
+		A[u] = 'a' + rand() % 26;
+		cout << " " << A[u];
+		if (u < k - 1)
+			cout << ';';
+	}
+	cout << "}" << endl;
+	cout << "Буквы, которые не повторяются: ";
+	for (u = 0; u <= k - 1; u++)
+	{
+		a = true;
+		for (i = 0; i <= k - 1; i++)
+		{
+			if (A[u] == A[i] && i != u)
+				a = false;
+		}
+		if (a == true)
+			cout << A[u] << ' ';
+	}
+}
