a4f35e0e38b75442672730ccd238d1d0af202d21
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 19:01:54 +0300

    Add files via upload

---
+++ doplaba6/2/2/2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	using namespace std;
+	setlocale(LC_ALL, "RUS");
+	int n, n1, s, p;
+	double S = 0;
+	cout << "Введите количество лет: ";
+	cin >> n;
+	cout << "Введите число %, на которое снижается цена на оборудование с каждым годом: ";
+	cin >> p;
+	cout << "Введите количество денег для закупки:" << endl;
+	for (n1 = 1; n1 <= n; n1++)
+	{
+		cin >> s;
+		S = s + S - S * p / 100;
+	}
+	cout << "общаая стоимость оборудования за " << n << " лет = " << S << endl;
+}
